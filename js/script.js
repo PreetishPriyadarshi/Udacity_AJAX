59440a69cd3577376c52e4764730f95f02201e21
Preetish Priyadarshi <preetish.pn2@gmail.com>
2017-07-28 03:05:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,8 +12,17 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    
 
     // YOUR CODE GOES HERE!
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ',' + cityStr;
+    
+    $greeting.text('So, you want to live at ' + address + '?' );
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600X400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+    
 
     return false;
 };
